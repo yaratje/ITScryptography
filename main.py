e78b31210d33c320422bfdde9af722d091a8636c
microm3 <micromeee@gmail.com>
2025-04-27 09:15:12 +0200

    wrap caesar code in method to reuse

--- main.py
+++ main.py
@@ -6,36 +6,48 @@ import math
 text = "CREAS"
 # text = "CZGGJOOCDNODNOVIOZSVHKGZ"
 
-#unigram up to 5-gram
-ngram_prob = generate_ngram_probs()
 
-#generates the word list but like 1 is the bottom one from the list in the paper
-word_list = gen_shift_list(text)
-
-
-#log probability ofword using the n-gram probs
-def word_prob(word, n):
-    probs = ngram_prob.get(n, {})
-    total_prob = 0.0 
-
-    for i in range(len(word) - n + 1):
-        gram = word[i:i+n]
-        prob = probs.get(gram, 1e-10)  #prob is very small
-        total_prob += prob
+def calculate_normalized_probs(text):
+    #unigram up to 5-gram
+    ngram_prob = generate_ngram_probs()
     
-    return total_prob
-
-for n in range (1,6):
-    unigram_probs = []
-    for word in word_list:
-        prob = word_prob(word,n)
-        unigram_probs.append((word,prob))
+    #generates the word list but like 1 is the bottom one from the list in the paper
+    word_list = gen_shift_list(text)
     
-    total = sum(prob for _, prob in unigram_probs)
-    normalized_probs = [(prob / total, word) for word, prob in unigram_probs]
-    normalized_probs.sort(key=lambda x: x[0], reverse=True)
-    print(f"n is {n}")
-    print(normalized_probs)
+    #log probability ofword using the n-gram probs
+    def word_prob(word, n):
+        probs = ngram_prob.get(n, {})
+        total_prob = 0.0
+        for i in range(len(word) - n + 1):
+            gram = word[i:i+n]
+            prob = probs.get(gram, 1e-10)
+            total_prob += prob
+        return total_prob
 
-print("This word decodes as:")
-print(normalized_probs[0])
+    all_normalized_probs = []
+
+    for n in range(1, 7):
+        unigram_probs = []
+        for word in word_list:
+            prob = word_prob(word, n)
+            unigram_probs.append((word, prob))
+
+        total = sum(prob for _, prob in unigram_probs)
+        if total == 0:
+            total = 1e-12
+            
+        normalized_probs = [(prob / total, word) for word, prob in unigram_probs]
+        normalized_probs.sort(key=lambda x: x[0], reverse=True)
+        all_normalized_probs.append((n, normalized_probs))
+
+    return all_normalized_probs
+
+if __name__ == "__main__":
+        
+    results = calculate_normalized_probs(text)
+    
+    for n, normalized_probs in results:
+        print(f"n is {n}")
+        print(normalized_probs)
+    print("This word decodes as:")
+    print(results[-1][1][0])  
